569134d2a6617c18f7275565f3786ea79d184f8f
maevet78 <190439mt@saintviator.com>
2018-08-07 16:19:05 +0000

    Done.

--- spec/meal_choice_spec.rb
+++ spec/meal_choice_spec.rb
@@ -1,51 +1,51 @@
-require 'spec_helper'
+# require 'spec_helper'
 
 
-describe "#snacks" do 
+# describe "#snacks" do 
 
-  it 'Returns the argument interpolated in the string' do
-    expect(snacks("egg sandwiches")).to eq("Any time, is the right time for egg sandwiches!" )
-  end
+#   it 'Returns the argument interpolated in the string' do
+#     expect(snacks("egg sandwiches")).to eq("Any time, is the right time for egg sandwiches!" )
+#   end
 
-    it 'Returns cheetos interpolated in the string when there is no argument' do
-    expect(snacks).to eq("Any time, is the right time for Cheetos!" )
-  end
+#     it 'Returns cheetos interpolated in the string when there is no argument' do
+#     expect(snacks).to eq("Any time, is the right time for Cheetos!" )
+#   end
 
-end
+# end
 
-describe "#breakfast" do 
+# describe "#breakfast" do 
 
-  it 'Returns the argument interpolated in the string' do
-    expect(breakfast("pancakes").downcase).to include("pancakes")
-  end
+#   it 'Returns the argument interpolated in the string' do
+#     expect(breakfast("pancakes").downcase).to include("pancakes")
+#   end
 
-    it 'Returns frosted flakes interpolated in the string when there is no argument' do
-    expect(breakfast).to include("frosted flakes")
-  end
+#     it 'Returns frosted flakes interpolated in the string when there is no argument' do
+#     expect(breakfast).to include("frosted flakes")
+#   end
 
-end
+# end
 
-describe "#lunch" do 
+# describe "#lunch" do 
 
-  it 'Returns the argument interpolated in the string' do
-    expect(lunch("peanut butter").downcase).to include("peanut butter")
-  end
+#   it 'Returns the argument interpolated in the string' do
+#     expect(lunch("peanut butter").downcase).to include("peanut butter")
+#   end
 
-    it 'Returns grilled cheese interpolated in the string when there is no argument' do
-    expect(lunch).to include("grilled cheese")
-  end
+#     it 'Returns grilled cheese interpolated in the string when there is no argument' do
+#     expect(lunch).to include("grilled cheese")
+#   end
 
-end
+# end
 
-describe "#dinner" do 
+# describe "#dinner" do 
 
-  it 'Returns the argument interpolated in the string' do
-    expect(dinner("broccoli").downcase).to include("broccoli")
-  end
+#   it 'Returns the argument interpolated in the string' do
+#     expect(dinner("broccoli").downcase).to include("broccoli")
+#   end
 
-    it 'Returns Salmon interpolated in the string when there is no argument' do
-    expect(dinner).to include("salmon")
-  end
+#     it 'Returns Salmon interpolated in the string when there is no argument' do
+#     expect(dinner).to include("salmon")
+#   end
 
-end
+# end
 
